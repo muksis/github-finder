9f1f2323dc1f4625303bedd52d4088993f572182
muksis <60219890+muksis@users.noreply.github.com>
2022-04-16 17:04:24 -0400

    Add UserItem

--- src/components/users/UserResults.jsx
+++ src/components/users/UserResults.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react';
 import Spinner from '../layout/Spinner';
+import UserItem from './UserItem';
 
 function UserResults() {
   const [users, setUsers] = useState([]);
@@ -10,11 +11,13 @@ function UserResults() {
   }, []);
 
   const fetchUsers = async () => {
-    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`, {
-      headers: {
-        Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
-      },
-    })
+    const response = await fetch(`${process.env.REACT_APP_GITHUB_URL}/users`
+    // , {
+    //   headers: {
+    //     Authorization: `token ${process.env.REACT_APP_GITHUB_TOKEN}`,
+    //   },
+    // }
+    )
 
     const data = await response.json()
 
@@ -26,7 +29,7 @@ function UserResults() {
     return (
       <div className='grid grid-cols-1 gap-8 xl:grid-cols-4 lg:grid-cols-3 md:grid-cols-2'>
         {users.map((user) => (
-          <h3>{user.login}</h3>
+          <UserItem key={user.id} user={user} />
         ))}
       </div>
     )
