e1dbe999a5d7fbae2130145ea3719b86c8dd8b4d
muksis <60219890+muksis@users.noreply.github.com>
2022-04-18 17:40:26 -0400

    Add get search results

--- src/components/users/UsersSearch.jsx
+++ src/components/users/UsersSearch.jsx
@@ -4,7 +4,7 @@ import GithubContext from "../../context/github/GithubContext";
 function UsersSearch() {
   const [text, setText] = useState('');
 
-  const {users} = useContext(GithubContext);
+  const {users, searchUsers} = useContext(GithubContext);
 
   const handleChange = (e) => setText(e.target.value);
 
@@ -14,7 +14,7 @@ function UsersSearch() {
       if (text === '') {
           alert('Please enter something')
       } else {
-          // @todo - search users
+          searchUsers(text);
 
           setText('');
       }
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -14,10 +14,15 @@ export const GithubProvider = ({children}) => {
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
 
-  // Get initial users  (testing purposes)
-  const fetchUsers = async () => {
+  // Get search results
+  const searchUsers = async (text) => {
     setLoading();
-    const response = await fetch(`${GITHUB_URL}/users`
+
+    const params = new URLSearchParams({
+      q: text
+    });
+
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`
     // , {
     //   headers: {
     //     Authorization: `token ${GITHUB_TOKEN}`,
@@ -25,11 +30,11 @@ export const GithubProvider = ({children}) => {
     // }
     );
 
-    const data = await response.json();
+    const {items} = await response.json();
 
     dispatch({
        type: 'GET_USERS',
-       payload: data, 
+       payload: items, 
     })
   }
 
@@ -40,7 +45,7 @@ export const GithubProvider = ({children}) => {
   return <GithubContext.Provider value={{
       users: state.users,
       loading: state.loading,
-      fetchUsers
+      searchUsers
   }}>
       {children}
   </GithubContext.Provider>
