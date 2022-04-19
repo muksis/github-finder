650745b0f36e9c2b325892b7a659fb10cc034326
muksis <60219890+muksis@users.noreply.github.com>
2022-04-19 14:05:14 -0400

    Add alert context

--- src/App.js
+++ src/App.js
@@ -5,27 +5,29 @@ import NotFound from './pages/NotFound';
 import Navbar from './components/layout/Navbar';
 import Footer from './components/Footer';
 import { GithubProvider } from './context/github/GithubContext';
+import { AlertProvider } from './context/alert/AlertContext';
 
 function App() {
   return (
     <GithubProvider>
+      <AlertProvider>
+        <Router>
+          <div className='flex flex-col justify-between h-screen'>
+            <Navbar />
 
-      <Router>
-        <div className='flex flex-col justify-between h-screen'>
-          <Navbar />
+            <main className='container mx-auto px-3 pb-12'>
+              <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/notfound' element={<NotFound />} />
+                <Route path='/*' element={<NotFound />} />
+              </Routes>
+            </main>
 
-          <main className='container mx-auto px-3 pb-12'>
-            <Routes>
-              <Route path='/' element={<Home />} />
-              <Route path='/about' element={<About />} />
-              <Route path='/notfound' element={<NotFound />} />
-              <Route path='/*' element={<NotFound />} />
-            </Routes>
-          </main>
-
-          <Footer />
-        </div> 
-      </Router>
+            <Footer />
+          </div> 
+        </Router>
+      </AlertProvider>
     </GithubProvider>
   );
 }
