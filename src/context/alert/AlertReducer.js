a1dd229c7c9e891b568a2058f56cb0d231c6d2e3
muksis <60219890+muksis@users.noreply.github.com>
2022-04-19 18:08:43 -0400

    Add single user

--- src/context/alert/AlertReducer.js
+++ src/context/alert/AlertReducer.js
@@ -1,5 +1,5 @@
 const AlertReducer = (state, action) => {
-  switch(action.type) {
+  switch (action.type) {
     case 'SET_ALERT':
       return action.payload;
     case 'REMOVE_ALERT':
--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -9,7 +9,8 @@ const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 export const GithubProvider = ({children}) => {
   const initialState = {
       users: [],
-      loading: false
+      user: {},
+      loading: false,
   }
 
   const [state, dispatch] = useReducer(githubReducer, initialState);
@@ -35,8 +36,32 @@ export const GithubProvider = ({children}) => {
     dispatch({
        type: 'GET_USERS',
        payload: items, 
-    })
-  }
+    });
+  };
+
+  // Get single user
+  const getUser = async (login) => {
+    setLoading();
+
+    const response = await fetch(`${GITHUB_URL}/users/users/:login`
+    // , {
+    //   headers: {
+    //     Authorization: `token ${GITHUB_TOKEN}`,
+    //   },
+    // }
+    );
+
+    if (response.status === 404) {
+      window.location = '/notfound';
+    } else {
+        const data = await response.json();
+    
+        dispatch({
+          type: 'GET_USER',
+          payload: data, 
+        });
+    }
+  };
 
   // Clear users from state
   const clearUsers = () => dispatch({type: 'CLEAR_USERS'});
@@ -48,8 +73,10 @@ export const GithubProvider = ({children}) => {
   return <GithubContext.Provider value={{
       users: state.users,
       loading: state.loading,
+      user: state.user,
       searchUsers,
       clearUsers,
+      getUser,
   }}>
       {children}
   </GithubContext.Provider>
