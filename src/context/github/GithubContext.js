75d168e5e6ae3d544af9c5b41286c4e1070214ee
muksis <60219890+muksis@users.noreply.github.com>
2022-04-21 14:18:16 -0400

    Fixing token issue

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -4,7 +4,7 @@ import githubReducer from "./GithubReducer";
 const GithubContext = createContext();
 
 const GITHUB_URL = process.env.REACT_APP_GITHUB_URL;
-// const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
 
 export const GithubProvider = ({children}) => {
   const initialState = {
@@ -24,13 +24,11 @@ export const GithubProvider = ({children}) => {
       q: text
     });
 
-    const response = await fetch(`${GITHUB_URL}/search/users?${params}`
-    // , {
-    //   headers: {
-    //     Authorization: `token ${GITHUB_TOKEN}`,
-    //   },
-    // }
-    );
+    const response = await fetch(`${GITHUB_URL}/search/users?${params}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    });
 
     const {items} = await response.json();
 
@@ -44,13 +42,11 @@ export const GithubProvider = ({children}) => {
   const getUser = async (login) => {
     setLoading();
 
-    const response = await fetch(`${GITHUB_URL}/users/${login}`
-    // , {
-    //   headers: {
-    //     Authorization: `token ${GITHUB_TOKEN}`,
-    //   },
-    // }
-    );
+    const response = await fetch(`${GITHUB_URL}/users/${login}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    });
 
     if (response.status === 404) {
       window.location = '/notfound';
@@ -73,13 +69,11 @@ export const GithubProvider = ({children}) => {
       per_page: 10,
     });
 
-    const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`
-    // , {
-    //   headers: {
-    //     Authorization: `token ${GITHUB_TOKEN}`,
-    //   },
-    // }
-    );
+    const response = await fetch(`${GITHUB_URL}/users/${login}/repos?${params}`, {
+      headers: {
+        Authorization: `token ${GITHUB_TOKEN}`,
+      },
+    });
 
     const data = await response.json();
 
