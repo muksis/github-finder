24fee2bdde5e2c35861056a56a6203e908d2d70b
muksis <60219890+muksis@users.noreply.github.com>
2022-06-26 17:31:10 -0400

    Remove extra braces in about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -6,9 +6,8 @@ function About() {
         A React app to search GitHub profiles and see profile details. This
         project is part of the
         <a href='https://www.udemy.com/course/modern-react-front-to-back/'>
-          {' '}
           React Front To Back
-        </a>{' '}
+        </a>
         Udemy course by
         <strong>
           <a href='https://traversymedia.com'> Brad Traversy</a>
